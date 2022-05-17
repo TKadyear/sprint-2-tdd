b37142688e20c48303d6dd2acfe54ce842a66a9f
TKadyear <tamarakadyear@gmail.com>
2022-05-17 16:54:01 +0200

    Added the function for the method isOccupied of the class Room

--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
+const getTimeStamp = (date) => new Date(date).getTime();
 class Room {
-  constructor() {
-
+  constructor({ name, bookings, rate, discount }) {
+    this.name = name; //String
+    this.bookings = bookings; //Array
+    this.rate = rate; //Number
+    this.discount = discount; //Number
   }
   isOccupied(date) {
-    return true;
+    const dateRequired = getTimeStamp(date);
+    const findRoom = [...this.bookings].find(room => {
+      const timeStampCheckIn = getTimeStamp(room.checkIn);
+      const timeStampCheckOut = getTimeStamp(room.checkOut);
+      if (timeStampCheckIn > dateRequired && dateRequired < timeStampCheckOut) {
+        return room;
+      };
+    })
+    return findRoom ? findRoom.name : false;
   }
   occupancyPercentage({ startDate, endDate }) {
   }
